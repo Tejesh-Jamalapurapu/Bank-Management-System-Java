064788c5e446cd25ec10a444c7d6c08dd7c3990a
Tejesh-Jamalapurapu <tejeshjamalapurapu23.com>
2026-02-08 12:35:50 +0530

    Day-4: Added validations

--- src/bankManagementSystem1/BankService.java
+++ src/bankManagementSystem1/BankService.java
@@ -6,50 +6,54 @@ import java.util.List;
 public class BankService {
 	private List<Account> accounts = new ArrayList<>();
 
-    // Create account
-    public void addAccount(Account account) {
-        accounts.add(account);
-        System.out.println("Account created successfully!");
-    }
+	public void addAccount(Account account) {
+		accounts.add(account);
+		System.out.println("Account created successfully!");
+	}
 
-    // Find account by account number
-    private Account findAccount(int accNo) {
-        for (Account acc : accounts) {
-            if (acc.getAccountNumber() == accNo) {
-                return acc;
-            }
-        }
-        return null;
-    }
+	private Account findAccount(int accNo) {
+		for (Account acc : accounts) {
+			if (acc.getAccountNumber() == accNo) {
+				return acc;
+			}
+		}
+		return null;
+	}
 
-    // Deposit
-    public void deposit(int accNo, double amount) {
-        Account acc = findAccount(accNo);
-        if (acc != null) {
-            acc.deposit(amount);
-        } else {
-            System.out.println("Account not found!");
-        }
-    }
+	public void deposit(int accNo, double amount) {
+		Account acc = findAccount(accNo);
+		if (acc == null) {
+			System.out.println("Account not found!");
+			return;
+		}
 
-    // Withdraw
-    public void withdraw(int accNo, double amount) {
-        Account acc = findAccount(accNo);
-        if (acc != null) {
-            acc.withdraw(amount);
-        } else {
-            System.out.println("Account not found!");
-        }
-    }
+		try {
+			acc.deposit(amount);
+		} catch (InvalidAmountException e) {
+			System.out.println(e.getMessage());
+		}
+	}
 
-    // Check balance
-    public void checkBalance(int accNo) {
-        Account acc = findAccount(accNo);
-        if (acc != null) {
-            System.out.println("Balance: " + acc.getBalance());
-        } else {
-            System.out.println("Account not found!");
-        }
-    }
+	public void withdraw(int accNo, double amount) {
+		Account acc = findAccount(accNo);
+		if (acc == null) {
+			System.out.println("Account not found!");
+			return;
+		}
 
+		try {
+			acc.withdraw(amount);
+		} catch (InvalidAmountException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+	public void checkBalance(int accNo) {
+		Account acc = findAccount(accNo);
+		if (acc != null) {
+			System.out.println("Balance: " + acc.getBalance());
+		} else {
+			System.out.println("Account not found!");
+		}
+	}
 }
